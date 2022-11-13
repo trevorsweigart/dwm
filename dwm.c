136bfa7e2a177b142ff28fd736f42b02c7c0c57d
Trevor <sweigart.trevor@protonmail.com>
2022-11-13 01:56:35 -0500

    Possible patch fix

--- dwm.c
+++ dwm.c
@@ -1785,7 +1785,7 @@ setup(void)
 	cursor[CurMove] = drw_cur_create(drw, XC_fleur);
 	/* init appearance */
 	scheme = ecalloc(LENGTH(colors) + 1, sizeof(Clr *));
-	scheme[LENGTH(colors)] = drw_scm_create(drw, colors[0], 3);
+	scheme[LENGTH(colors)] = drw_scm_create(drw, colors[0], alphas[0], 3);
 	for (i = 0; i < LENGTH(colors); i++)
 		scheme[i] = drw_scm_create(drw, colors[i], alphas[i], 3);
 	/* init bars */
