4d1814f5e6ebe4af978c52a53465713ce863885b
Trevor <sweigart.trevor@protonmail.com>
2022-11-13 04:21:15 -0500

    Add barheight patch

--- config.def.h
+++ config.def.h
@@ -15,6 +15,7 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;     /* 0 means no bar */
 static const int topbar             = 1;     /* 0 means bottom bar */
+static const int user_bh            = 2;        /* 2 is the default spacing around the bar's font */
 static const char *fonts[]          = { "monospace:size=10" };
 static const char dmenufont[]       = "monospace:size=10";
 static const char col_gray1[]       = "#222222";
